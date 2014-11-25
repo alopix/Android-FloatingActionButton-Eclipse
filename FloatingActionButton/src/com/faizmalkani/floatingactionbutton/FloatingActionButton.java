02f6f191ecafdac53dedc9dae0f58b8d00415860
Dustin Steiner <dustin.steiner@fh-hagenberg.at>
2014-11-25 11:12:22 +0100

    app compat and color fix

--- FloatingActionButton/src/com/faizmalkani/floatingactionbutton/FloatingActionButton.java
+++ FloatingActionButton/src/com/faizmalkani/floatingactionbutton/FloatingActionButton.java
@@ -24,131 +24,141 @@ import com.nineoldandroids.view.ViewHelper;
 
 public class FloatingActionButton extends View {
 
-    private final Interpolator mInterpolator = new AccelerateDecelerateInterpolator();
-    private final Paint mButtonPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private final Paint mDrawablePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private Bitmap mBitmap;
-    private int mColor;
-    private boolean mHidden = false;
-    /**
-     * The FAB button's Y position when it is displayed.
-     */
-    private float mYDisplayed = -1;
-    /**
-     * The FAB button's Y position when it is hidden.
-     */
-    private float mYHidden = -1;
+	private final Interpolator mInterpolator = new AccelerateDecelerateInterpolator();
+	private final Paint mButtonPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+	private final Paint mDrawablePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+	private Bitmap mBitmap;
+	private int mColor;
+	private boolean mHidden = false;
+	/**
+	 * The FAB button's Y position when it is displayed.
+	 */
+	private float mYDisplayed = -1;
+	/**
+	 * The FAB button's Y position when it is hidden.
+	 */
+	private float mYHidden = -1;
 
-    public FloatingActionButton(Context context) {
-        this(context, null);
-    }
+	public FloatingActionButton(Context context) {
+		this(context, null);
+	}
 
-    public FloatingActionButton(Context context, AttributeSet attributeSet) {
-        this(context, attributeSet, 0);
-    }
+	public FloatingActionButton(Context context, AttributeSet attributeSet) {
+		this(context, attributeSet, 0);
+	}
 
+	public FloatingActionButton(Context context, AttributeSet attrs,
+			int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
 
-    public FloatingActionButton(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+		TypedArray a = getContext().obtainStyledAttributes(attrs,
+				R.styleable.FloatingActionButton);
+		mColor = a.getColor(R.styleable.FloatingActionButton_android_color,
+				Color.WHITE);
+		mButtonPaint.setStyle(Paint.Style.FILL);
+		mButtonPaint.setColor(mColor);
+		float radius, dx, dy;
+		radius = a.getFloat(R.styleable.FloatingActionButton_shadowRadius,
+				10.0f);
+		dx = a.getFloat(R.styleable.FloatingActionButton_shadowDx, 0.0f);
+		dy = a.getFloat(R.styleable.FloatingActionButton_shadowDy, 3.5f);
+		int color = a.getInteger(R.styleable.FloatingActionButton_shadowColor,
+				Color.argb(100, 0, 0, 0));
+		mButtonPaint.setShadowLayer(radius, dx, dy, color);
 
-        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.FloatingActionButton);
-        mColor = a.getColor(R.styleable.FloatingActionButton_color, Color.WHITE);
-        mButtonPaint.setStyle(Paint.Style.FILL);
-        mButtonPaint.setColor(mColor);
-        float radius, dx, dy;
-        radius = a.getFloat(R.styleable.FloatingActionButton_shadowRadius, 10.0f);
-        dx = a.getFloat(R.styleable.FloatingActionButton_shadowDx, 0.0f);
-        dy = a.getFloat(R.styleable.FloatingActionButton_shadowDy, 3.5f);
-        int color = a.getInteger(R.styleable.FloatingActionButton_shadowColor, Color.argb(100, 0, 0, 0));
-        mButtonPaint.setShadowLayer(radius, dx, dy, color);
+		Drawable drawable = a
+				.getDrawable(R.styleable.FloatingActionButton_drawable);
+		if (null != drawable) {
+			mBitmap = ((BitmapDrawable) drawable).getBitmap();
+		}
+		setWillNotDraw(false);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+			setLayerType(View.LAYER_TYPE_SOFTWARE, null);
 
-        Drawable drawable = a.getDrawable(R.styleable.FloatingActionButton_drawable);
-        if (null != drawable) {
-            mBitmap = ((BitmapDrawable) drawable).getBitmap();
-        }
-        setWillNotDraw(false);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
-            setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+		WindowManager mWindowManager = (WindowManager) context
+				.getSystemService(Context.WINDOW_SERVICE);
+		Display display = mWindowManager.getDefaultDisplay();
+		Point size = new Point();
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+			display.getSize(size);
+			mYHidden = size.y;
+		} else
+			mYHidden = display.getHeight();
+	}
 
-        WindowManager mWindowManager = (WindowManager)
-                context.getSystemService(Context.WINDOW_SERVICE);
-        Display display = mWindowManager.getDefaultDisplay();
-        Point size = new Point();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            display.getSize(size);
-            mYHidden = size.y;
-        } else mYHidden = display.getHeight();
-    }
+	public static int darkenColor(int color) {
+		float[] hsv = new float[3];
+		Color.colorToHSV(color, hsv);
+		hsv[2] *= 0.8f;
+		return Color.HSVToColor(hsv);
+	}
 
-    public static int darkenColor(int color) {
-        float[] hsv = new float[3];
-        Color.colorToHSV(color, hsv);
-        hsv[2] *= 0.8f;
-        return Color.HSVToColor(hsv);
-    }
+	public void setColor(int color) {
+		mColor = color;
+		mButtonPaint.setColor(mColor);
+		invalidate();
+	}
 
-    public void setColor(int color) {
-        mColor = color;
-        mButtonPaint.setColor(mColor);
-        invalidate();
-    }
+	public void setDrawable(Drawable drawable) {
+		mBitmap = ((BitmapDrawable) drawable).getBitmap();
+		invalidate();
+	}
 
-    public void setDrawable(Drawable drawable) {
-        mBitmap = ((BitmapDrawable) drawable).getBitmap();
-        invalidate();
-    }
+	@Override
+	protected void onDraw(Canvas canvas) {
+		canvas.drawCircle(getWidth() / 2, getHeight() / 2,
+				(float) (getWidth() / 2.6), mButtonPaint);
+		if (null != mBitmap) {
+			canvas.drawBitmap(mBitmap, (getWidth() - mBitmap.getWidth()) / 2,
+					(getHeight() - mBitmap.getHeight()) / 2, mDrawablePaint);
+		}
+	}
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        canvas.drawCircle(getWidth() / 2, getHeight() / 2, (float) (getWidth() / 2.6), mButtonPaint);
-        if (null != mBitmap) {
-            canvas.drawBitmap(mBitmap, (getWidth() - mBitmap.getWidth()) / 2,
-                    (getHeight() - mBitmap.getHeight()) / 2, mDrawablePaint);
-        }
-    }
+	@Override
+	protected void onLayout(boolean changed, int left, int top, int right,
+			int bottom) {
+		// Perform the default behavior
+		super.onLayout(changed, left, top, right, bottom);
 
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        // Perform the default behavior
-        super.onLayout(changed, left, top, right, bottom);
+		// Store the FAB button's displayed Y position if we are not already
+		// aware of it
+		if (mYDisplayed == -1) {
 
-        // Store the FAB button's displayed Y position if we are not already aware of it
-        if (mYDisplayed == -1) {
+			mYDisplayed = ViewHelper.getY(this);
+		}
+	}
 
-            mYDisplayed = ViewHelper.getY(this);
-        }
-    }
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		int color;
+		if (event.getAction() == MotionEvent.ACTION_UP) {
+			color = mColor;
+		} else {
+			color = darkenColor(mColor);
+		}
+		mButtonPaint.setColor(color);
+		invalidate();
+		return super.onTouchEvent(event);
+	}
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        int color;
-        if (event.getAction() == MotionEvent.ACTION_UP) {
-            color = mColor;
-        } else {
-            color = darkenColor(mColor);
-        }
-        mButtonPaint.setColor(color);
-        invalidate();
-        return super.onTouchEvent(event);
-    }
+	public void hide(boolean hide) {
+		// If the hidden state is being updated
+		if (mHidden != hide) {
 
-    public void hide(boolean hide) {
-        // If the hidden state is being updated
-        if (mHidden != hide) {
+			// Store the new hidden state
+			mHidden = hide;
 
-            // Store the new hidden state
-            mHidden = hide;
+			// Animate the FAB to it's new Y position
+			ObjectAnimator animator = ObjectAnimator.ofFloat(this, "y",
+					mHidden ? mYHidden : mYDisplayed).setDuration(500);
+			animator.setInterpolator(mInterpolator);
+			animator.start();
+		}
+	}
 
-            // Animate the FAB to it's new Y position
-            ObjectAnimator animator = ObjectAnimator.ofFloat(this, "y", mHidden ? mYHidden : mYDisplayed).setDuration(500);
-            animator.setInterpolator(mInterpolator);
-            animator.start();
-        }
-    }
-
-    public void listenTo(AbsListView listView) {
-        if (null != listView) {
-            listView.setOnScrollListener(new DirectionScrollListener(this));
-        }
-    }
+	public void listenTo(AbsListView listView) {
+		if (null != listView) {
+			listView.setOnScrollListener(new DirectionScrollListener(this));
+		}
+	}
 }
